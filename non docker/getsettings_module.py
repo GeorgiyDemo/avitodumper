cc63b20cc6c905d79b9e9c72235974f0dedf78e4
Georgiy Demo <demenchuk.george@protonmail.com>
2019-08-17 20:36:34 +0300

    Разделение docker и non docker на 2 бранча

--- non docker/getsettings_module.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-
-def get_settings():
-    """
-        Функция для чтения конфигов с yaml
-    """
-    with open("./settings.yml", 'r') as stream:
-        return yaml.safe_load(stream)
